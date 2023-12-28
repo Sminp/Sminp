00346816929b8d3a5f7869da73fcd61aa61e5735
Sminp <0102262@gmail.com>
2023-12-28 05:22:59 +0000

    박소민 필요없는 파일 리팩토링

--- capd-ParkSomin/static/src/pages/WritePage.js
+++ capd-ParkSomin/static/src/pages/WritePage.js
@@ -1,11 +1,7 @@
 import EditorContainer from '../containers/write/EditorContainer';
-// import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 export default function WritePage() {
   return (
-    <>
       <EditorContainer />
-      {/* <TagBoxContainer /> */}
-    </>
   );
 }
